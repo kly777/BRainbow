2ae883bff04ae88fe47356f88776aada85029716
kly777 <kly777777777@gmail.com>
2026-05-27 23:51:39 +0800

    Generate random favicon on app mount

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,11 +1,14 @@
 import { Router } from "@solidjs/router";
+import { onMount } from "solid-js";
 import { render } from "solid-js/web";
 import { AuthProvider } from "./auth/context.tsx";
 import Layout from "./Layout.tsx";
 import { ROUTES, toRouteDefs } from "./routes.ts";
+import { generateIcon } from "./lib/icon.ts";
 import "@/global.css";
 
 function App() {
+    onMount(() => generateIcon());
     return (
         <AuthProvider>
             <Router root={Layout}>{toRouteDefs(ROUTES)}</Router>

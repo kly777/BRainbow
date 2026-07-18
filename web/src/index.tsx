bb48da3a261fd32d43c67057bf26be2130942589
kly777 <kly777777777@gmail.com>
2026-07-18 13:45:29 +0800

    fix:router

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,29 +2,26 @@ import { Router } from "@solidjs/router";
 import { onMount } from "solid-js";
 import { render } from "solid-js/web";
 import { AuthProvider } from "./auth/context.tsx";
-import ErrorBoundary from "./components/ErrorBoundary.tsx";
 import Layout from "./Layout.tsx";
 import { generateIcon } from "./lib/icon.ts";
 import { ROUTES, toRouteDefs } from "./routes.ts";
 import "@/global.css";
 
 function App() {
-	onMount(() => generateIcon());
-	return (
-		<ErrorBoundary>
-			<AuthProvider>
-				<Router root={Layout}>{toRouteDefs(ROUTES)}</Router>
-			</AuthProvider>
-		</ErrorBoundary>
-	);
+    onMount(() => generateIcon());
+    return (
+        <AuthProvider>
+            <Router root={Layout}>{toRouteDefs(ROUTES)}</Router>
+        </AuthProvider>
+    );
 }
 
 const root = document.getElementById("app");
 if (!root) {
-	const el = document.createElement("div");
-	el.id = "app";
-	document.body.appendChild(el);
-	render(() => <App />, el);
+    const el = document.createElement("div");
+    el.id = "app";
+    document.body.appendChild(el);
+    render(() => <App />, el);
 } else {
-	render(() => <App />, root);
+    render(() => <App />, root);
 }

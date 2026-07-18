ce2f6d38182f6732bf250ef114b105db159de282
kly777 <kly777777777@gmail.com>
2026-07-18 10:17:42 +0800

    Refactor card masonry to use flexbox layout

--- web/src/features/card/ui/CardsGrid.tsx
+++ web/src/features/card/ui/CardsGrid.tsx
@@ -33,7 +33,7 @@ const CardsGrid: Component<CardsGridProps> = (props) => {
 	});
 
 	return (
-		<div style={{ flex: 1, "min-height": 0, "overflow-y": "auto" }}>
+		<div style={{ flex: 1, "min-height": 0, "overflow-y": "auto", display: "flex", "flex-direction": "column" }}>
 			{props.showFilters !== false && (
 				<CardFilter
 					onSearch={props.onSearch}

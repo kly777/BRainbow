21dab422c117af975cf5e228bb8c5611c051bacd
kly777 <kly777777777@gmail.com>
2026-08-15 12:03:40 +0800

    fmt: fix vite alias indentation

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -147,7 +147,7 @@ export default defineConfig(({ command, mode }) => {
 
 		resolve: {
 			alias: {
-					"@app": fileURLToPath(new URL("./src/app", import.meta.url)),
+				"@app": fileURLToPath(new URL("./src/app", import.meta.url)),
 				"@modules": fileURLToPath(new URL("./src/modules", import.meta.url)),
 				"@components": fileURLToPath(
 					new URL("./src/components", import.meta.url),
